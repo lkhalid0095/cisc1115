75db6632a38751fe1df3a73fd8c823dae376975b
Lubna <lkhalid0095@bths.edu>
2019-02-01 15:55:04 -0500

    /n

--- Cisc1115-Lubna Khalid/src/assignment_0/HelloWorld.java
+++ Cisc1115-Lubna Khalid/src/assignment_0/HelloWorld.java
@@ -3,8 +3,7 @@ package assignment_0;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
-		System.out.println("It's Lubna Khalid.");
+		System.out.println("Hello World!\nIt's Lubna Khalid.");
 
 	}
 
