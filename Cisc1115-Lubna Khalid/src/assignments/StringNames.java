56dd543fa21c54bb91bad7afb46a023180ff5487
lkhalid0095 <31857027+lkhalid0095@users.noreply.github.com>
2019-05-02 11:39:48 -0400

    Update StringNames.java

--- Cisc1115-Lubna Khalid/src/assignments/StringNames.java
+++ Cisc1115-Lubna Khalid/src/assignments/StringNames.java
@@ -1,5 +1,6 @@
 package assignments;
 
+
 import java.io.File;
 import java.util.Scanner;
 
@@ -22,107 +23,108 @@ import java.util.Scanner;
  */
  
 public class StringNames {
-	final static int SIZE = 15; //if i change the size to 15 it gives me an error? 
-	
-	// prints name in three different ways, prints a list of names alphabetically
-	public static void main(String[] args) throws Exception {
-		
-		Scanner sc = new Scanner(System.in);
-		printTable();
-		String[] names = new String[SIZE] ; 
-		nameArray(names);
-		String first = "";
-		String last = "";
-		String dashName = "";
-		String[] lastNames = new String[SIZE];
-		for(int i =0;i<SIZE; i++) {
-			first = names[i].substring(0, names[i].indexOf(" "));
-			last = names[i].substring(names[i].indexOf(" ")+1);
-			lastNames[i] = last;
-			dashName = printName(first,last);
-			printRow(first,last,dashName);
-			
-		}
-		System.out.println("\nThe list of the last names in the list are:");
-		printArray(lastNames);
-		System.out.println("\nEnter a last name to see if it's in the list. ");
-		String str = sc.next();
-		if(findLast(str,lastNames)) {
-			System.out.println("We found a match in the list!");
-		}else {
-			System.out.println("Sorry, we didn't find a matching name in the list.");
-		}
-		System.out.println("\nThe list in alphabetical order is: ");
-		bubbleSort(lastNames);
-		printArray(lastNames);
-		sc.close();
-	}
+ final static int SIZE = 15; //if i change the size to 15 it gives me an error? 
+ 
+ // prints name in three different ways, prints a list of names alphabetically
+ public static void main(String[] args) throws Exception {
+  
+  Scanner sc = new Scanner(System.in);
+  printTable();
+  String[] names = new String[SIZE] ; 
+  int count = nameArray(names);
+  String first = "";
+  String last = "";
+  String dashName = "";
+  String[] lastNames = new String[SIZE];
+  for(int i =0;i<count; i++) {
+   first = names[i].substring(0, names[i].indexOf(" "));
+   last = names[i].substring(names[i].indexOf(" ")+1);
+   lastNames[i] = last;
+   dashName = printName(first,last);
+   printRow(first,last,dashName);
+   
+  }
+  System.out.println("\nThe list of the last names in the list are:");
+  printArray(lastNames);
+  System.out.println("\nEnter a last name to see if it's in the list. ");
+  String str = sc.next();
+  if(findLast(str,lastNames)) {
+   System.out.println("We found a match in the list!");
+  }else {
+   System.out.println("Sorry, we didn't find a matching name in the list.");
+  }
+  System.out.println("\nThe list in alphabetical order is: ");
+  bubbleSort(lastNames);
+  printArray(lastNames);
+  sc.close();
+ }
 
-	// sorts array of last names in a non ascending order. (alphabetical order)
-	private static void bubbleSort(String[] lastNames) {
-		int pass = 0;
-		boolean swapped = false;
-		do {
-			pass++;
-			swapped = false;
-			for (int pos = 0; pos < SIZE- pass; pos++)
-			if (lastNames[pos].compareTo(lastNames[pos+1]) > 0) {
-			String temp = lastNames[pos];
-			lastNames[pos] = lastNames[pos+1];
-			lastNames[pos+1] = temp;
-			swapped = true;
-			}
-			} while(swapped == true);
-	}
-	
-	// determines if last name is in the array.
-	private static boolean findLast(String str, String[] lastNames) {
-		for(int i = 0;i<SIZE;i++) {
-			if(lastNames[i].contains(str)) {
-				return true;
-			}
-		}
-		return false;
-	}
+ // sorts array of last names in a non ascending order. (alphabetical order)
+ private static void bubbleSort(String[] lastNames) {
+  int pass = 0;
+  boolean swapped = false;
+  do {
+   pass++;
+   swapped = false;
+   for (int pos = 0; pos < SIZE- pass; pos++)
+   if (lastNames[pos].compareTo(lastNames[pos+1]) > 0) {
+   String temp = lastNames[pos];
+   lastNames[pos] = lastNames[pos+1];
+   lastNames[pos+1] = temp;
+   swapped = true;
+   }
+   } while(swapped == true);
+ }
+ 
+ // determines if last name is in the array.
+ private static boolean findLast(String str, String[] lastNames) {
+  for(int i = 0;i<SIZE;i++) {
+   if(lastNames[i].contains(str)) {
+    return true;
+   }
+  }
+  return false;
+ }
 
-	// prints array of last names
-	private static void printArray(String[] lastNames) {
-		for(int i =0;i<SIZE;i++) {
-			System.out.println(lastNames[i]);
-		}
-	}
+ // prints array of last names
+ private static void printArray(String[] lastNames) {
+  for(int i =0;i<SIZE;i++) {
+   System.out.println(lastNames[i]);
+  }
+ }
 
-	// prints row in the chart with "first last", "last,first","first--last"
-	private static void printRow(String first, String last, String dashName) {
-		System.out.printf("%-10s %-11s\t%-10s,%-10s \t %s \n",first,last,first,last,dashName);
-	}
+ // prints row in the chart with "first last", "last,first","first--last"
+ private static void printRow(String first, String last, String dashName) {
+  System.out.printf("%-10s %-11s\t%-10s,%-10s \t %s \n",first,last,first,last,dashName);
+ }
 
-	// prints first---last 
-	private static String printName(String first, String last) {
-		String dash = first+"---"+last;
-		return dash;
-	}
+ // prints first---last 
+ private static String printName(String first, String last) {
+  String dash = first+"---"+last;
+  return dash;
+ }
 
-	// read full names into an array
-	private static void nameArray(String[] names) throws Exception {
-		int count = 0;
-		Scanner in = new Scanner(new File("names.txt"));
-		while(in.hasNext()&& count< SIZE) {
-			for(int i =0; i< SIZE;i++) {
-				names[i] = in.nextLine();
-				count++;
-			}
-		}
-		in.close();
-	}
+ // read full names into an array
+ private static int nameArray(String[] names) throws Exception {
+  int count = 0;
+  Scanner in = new Scanner(new File("names.txt"));
+  while(in.hasNext()&& count< SIZE) {
+    names[count] = in.nextLine();
+    count++;
+  }
+  in.close();
+  return count;
+  
+ }
 
-	// prints heading of table
-	private static void printTable() {
-		System.out.println("\t\tList of People\t");
-		
-	}
+ // prints heading of table
+ private static void printTable() {
+  System.out.println("\t\tList of People\t");
+  
+ }
 
 }
+
 /** OUTPUT
  	List of People
 Stephanie  Guerrero   	Stephanie ,Guerrero   	 Stephanie---Guerrero 
